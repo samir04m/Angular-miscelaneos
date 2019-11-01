1b190483d387add1574f575caa66b9ed2fc7299a
Samir Mejia <samir04m@gmail.com>
2019-11-01 17:35:56 -0500

    Botones par aaumentar y disminuir el tamaño del texto

--- src/app/components/ng-styles/ng-styles.component.ts
+++ src/app/components/ng-styles/ng-styles.component.ts
@@ -3,15 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ng-styles',
   template: `
-    <p [ngStyle]="{ 'font-size': tamano+'px' }">
+    <p [style.fontSize.px]="tamano">
       Hola mundo.. esta es un tag
     </p>
+
+    <button class="btn btn-primary" (click)="tamano = tamano+5">
+      <i class="fa fa-plus"></i>
+     </button>
+     <button class="btn btn-primary" (click)="tamano = tamano-5">
+       <i class="fa fa-minus"></i>
+      </button>
+
   `,
   styles: []
 })
 export class NgStylesComponent implements OnInit {
 
-   tamano:number = 20;
+   tamano:number = 25;
 
   constructor() { }
 
